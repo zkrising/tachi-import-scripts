2eb70fc1ea71092b887a862603be18730fc5dfb0
zkldi <20380519+zkldi@users.noreply.github.com>
2021-11-29 18:05:54 +0000

    partially revert add new configs

--- src/node/util/tachi-info.ts
+++ src/node/util/tachi-info.ts
@@ -1,19 +1,8 @@
-import { TIS_CONFIG } from "./config";
-
-export const BokutachiConfig = {
-	name: "Bokutachi",
-	baseUrl: "https://bokutachi.xyz",
-	clientUrl: "https://bokutachi.xyz",
-	clientID: "todo",
-};
-
-export const BokutachiStagingConfig = {
-	name: "Bokutachi Staging",
+const TachiConfig = {
+	name: "Tachi",
 	baseUrl: "https://staging.bokutachi.xyz",
 	clientUrl: "https://staging.bokutachi.xyz",
-	clientID: "CI8c4eae44862e6d3b753eef2d2d859ac51d8c516b1",
+	clientID: "CI8c4eae44862e6d3b753eef2d2d859ac51d8c516b",
 };
-
-const TachiConfig = TIS_CONFIG.staging ? BokutachiStagingConfig : BokutachiConfig;
 
 export default TachiConfig;
