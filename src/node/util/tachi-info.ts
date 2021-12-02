32277acdb80d09223e28c87c1c2883abe280db63
zkldi <20380519+zkldi@users.noreply.github.com>
2021-12-02 00:07:46 +0000

    Add Bokutachi Config

--- src/node/util/tachi-info.ts
+++ src/node/util/tachi-info.ts
@@ -2,7 +2,7 @@ export const BokutachiConfig = {
 	name: "Bokutachi",
 	baseUrl: "https://bokutachi.xyz",
 	clientUrl: "https://bokutachi.xyz",
-	clientID: "todo",
+	clientID: "CI18c4ebe4297a9e66960ad7b7bc88e91ace634ef8",
 };
 
 export const BokutachiStagingConfig = {
@@ -12,6 +12,7 @@ export const BokutachiStagingConfig = {
 	clientID: "CI8c4eae44862e6d3b753eef2d2d859ac51d8c516b1",
 };
 
+// Get config from env var just incase someone wants to use staging.
 export function GetTachiConfig() {
 	const TachiConfig = process.env.STAGING ? BokutachiStagingConfig : BokutachiConfig;
 
