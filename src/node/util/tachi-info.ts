3f97889286d5048326153369c3ea1074ee15efaa
Yaroslav Chvanov <yaroslav.chvanov@gmail.com>
2024-11-22 20:26:41 +0300

    refactor: drop support for STAGING
    staging.bokutachi.xyz is NXDOMAIN and staging.boku.tachi.ac doesn't
    work.

--- src/node/util/tachi-info.ts
+++ src/node/util/tachi-info.ts
@@ -1,20 +1,10 @@
-export const BokutachiConfig = {
+const BokutachiConfig = {
 	name: "Bokutachi",
 	baseUrl: "https://bokutachi.xyz",
 	clientUrl: "https://bokutachi.xyz",
 	clientID: "CI18c4ebe4297a9e66960ad7b7bc88e91ace634ef8",
 };
 
-export const BokutachiStagingConfig = {
-	name: "Bokutachi Staging",
-	baseUrl: "https://staging.bokutachi.xyz",
-	clientUrl: "https://staging.bokutachi.xyz",
-	clientID: "CI8c4eae44862e6d3b753eef2d2d859ac51d8c516b1",
-};
-
-// Get config from env var just incase someone wants to use staging.
 export function GetTachiConfig() {
-	const TachiConfig = process.env.STAGING ? BokutachiStagingConfig : BokutachiConfig;
-
-	return TachiConfig;
+	return BokutachiConfig;
 }
